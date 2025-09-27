8ebabe2a870c36864441a4f7d32ca3b00558b9bb
masonnicoletti <mason.f.nicoletti@gmail.com>
2025-09-27 15:07:10 -0400

    Load step modifications

--- load.py
+++ load.py
@@ -5,7 +5,7 @@ import time
 
 logging.basicConfig(
     level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s',
-    filename='load.log'
+    filename='logs/load.log'
 )
 logger = logging.getLogger(__name__)
 
