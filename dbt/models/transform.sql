140032a1ed686d9f54162f14ee533d513297e97a
masonnicoletti <mason.f.nicoletti@gmail.com>
2025-09-28 18:13:50 -0400

    dbt troubleshooting

--- dbt/models/transform.sql
+++ dbt/models/transform.sql
@@ -1,10 +1,9 @@
 {{ config(materialized='table') }}
 
-# Calculate CO2 (kg) per trip
+-- Calculate CO2 (kg) per trip
 SELECT 
     y.*,
     (y.trip_distance * v.co2_grams_per_mile) / 1000.0 AS trip_co2_kgs
-FROM {{ ref('yellow_tripdata') }} y
-JOIN {{ ref('vehicle_emissions') }} v
+FROM {{ source('trip_data', 'yellow_tripdata') }} y
+JOIN {{ source('trip_data', 'vehicle_emissions') }} v
 ON v.vehicle_type = 'yellow_taxi'
-
